28da6fd58b5004194faa0771ab8055cb2f8b2aab
edunando <edu.dev@outlook.com.br>
2022-06-01 21:14:01 -0300

    Arquivo de Configuração

--- Backup-Remoto/funcoes.sh
+++ Backup-Remoto/funcoes.sh
@@ -3,7 +3,7 @@
 #Listar Backups que já estão salvos
 function Backups_Disponiveis(){
 	clear
-	echo "Backups Disponíveis\n"
+	echo "Backups Disponíveis"
 	ls "${PWD}/Backups/"
 }
 
@@ -17,10 +17,15 @@ function Realizar_Backup(){
 	user=$(cat ips.txt | grep PC-$pc | awk '{print $2}')
 	ip=$(cat ips.txt | grep PC-$pc | awk '{print $3}')
 	read -p 'Qual pasta você deseja fazer o backup?' pasta
-	mkdir "${PWD}/Backups/PC-$pc"
+	mkdir "${PWD}/Backups/PC-$pc" &> /dev/null
 	scp -r "$user@$ip:$pasta" "${PWD}/Backups/PC-$pc"
-	comp=$(tar -czvf $arquivo_backup "${PWD}/Backups/PC-$pc/")
+	tar -czvf $arquivo_backup "${PWD}/Backups/PC-$pc/$(basename $pasta)"
+	clear
+	rm -rf "${PWD}/Backups/PC-$pc/$(basename $pasta)"
+	cp $arquivo_backup "./Backups/PC-$pc/"
+	rm $arquivo_backup
 	echo "Backup Salvo em ${PWD}/Backups/PC-$pc"
+	echo ""
 }
 #Cadastrar computadores disponíveis para backup
 function Cadastrar_PC(){
