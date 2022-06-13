f9210649767f59211573c5ea754e678364d231fd
edunando <edu.dev@outlook.com.br>
2022-06-12 21:10:22 -0300

    Programa Principal

--- Backup-Remoto/main.sh
+++ Backup-Remoto/main.sh
@@ -12,7 +12,7 @@ while true;do
 	echo -e "Escolha uma das opções a seguir:\n"
 	
 	#Opções
-	echo "1- Configurações SSH"
+	echo "1- Mensagem de Boas vindas"
 	echo "2- Selecionar Dispositivo e iniciar Processo de Backup Remoto"
 	echo "3- Listar dispositivos disponíveis"
 	echo "4- Adicionar dispositivo remoto para backup"
@@ -22,35 +22,9 @@ while true;do
 
 	read -p "Selecione uma opção: " opcao
 	case $opcao in
-		"1")	limpar_tela
-			echo "a) Verificar se há um SSH instalado"
-			echo "b) Instalar o SSH"
-			echo "c) Verificar se o SSH está rodando"
-			echo "d) Inicializar o SSH"
-			echo "e) Parar o SSH"
-			echo "f) Sair das configurações SSH"
-			read -p "Selecione uma opção: " ssh_opt
 
-			case $ssh_opt in
-				"a"|"A") limpar_tela
-				       	 verifica_ssh ;;
-
-				"b"|"B") limpar_tela
-					 instalar_ssh ;;
-
-				"c"|"C") limpar_tela
-				         verificar_ssh_status ;;
-
-				"d"|"D") limpar_tela
-				       	 iniciar_ssh ;;
-
-				"e"|"E") limpar_tela
-			                 interromper_ssh ;;
-
-			        "f"|"F") limpar_tela
-					 echo "Voltando ao MENU PRINCIPAL" ;;
-		 			 
-			esac;;
+		"1") limpar_tela
+		     echo 'Tu és time de tradição, raça, amor e paixão. Oh, meu Mengo!';;
 		"2") limpar_tela
 		     Realizar_Backup;;
 	     	"3") Dispositivos_on;;
